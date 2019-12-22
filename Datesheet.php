d78b891296208d30dbfc87ba414c91833546eb83
misbahmalik11 <57314390+misbahmalik11@users.noreply.github.com>
2019-12-22 02:41:50 -0800

    Add files via upload

---
+++ Datesheet.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Datesheet extends Model
+{
+    protected $table='datesheet';
+}
---
+++ Marks.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Marks extends Model
+{
+    
+    protected $table='studentmarks';
+    public $timestamps=null;
+
+}
