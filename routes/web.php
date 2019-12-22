4d0e3084507a417a51d5363d5ed6c9d31bfa3a8d
misbahmalik11 <57314390+misbahmalik11@users.noreply.github.com>
2019-12-22 02:45:45 -0800

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/', 'pagesController@getindex');
+Route::get('/index', 'pagesController@getindex');
+
+Route::get('/attendence', 'pagesController@getattendence');
+Route::get('/datesheet','Datesheetcontroller@getdatesheet');
+Route::get('/home', 'pagesController@gethome');
+Route::get('/marksheet', 'maksController@getMarks');
+Route::get('/student', 'studentController@getStudentDetails');
+Route::get('/teacher', 'pagesController@getteacher');
+Route::resource('studentdetails', 'studentController');
+Route::get('/teacherquiz', 'pagesController@setquiz');
+Route::get('/teachersetmarks', 'pagesController@setmarks');
+Route::get('/teacherlogin', 'pagesController@getteacherlogin');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
